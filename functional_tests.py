741451fa955b8912ff4e92559dc6a1aefe265e12
Juan Pablo Rabino <juan.rabino@globant.com>
2017-04-27 16:30:53 -0300

    Committer: Juan Pablo Rabino <juan.rabino@globant.com>
     On branch master
     Changes to be committed:
    	modified:   functional_tests.py
    	deleted:    geckodriver.log
    
     Untracked files:
    	.idea/
    	geckodriver.log
    	superlists/__pycache__/

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,44 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the Test!')
+
+        #Ingresar un item nuevo
+
+        #Escribe "comprar tacho de basura" en un text box
+
+        #cuando apreta 'Enter' se agraga el item con la leyenda
+        # 1: Comprar tacho de basura
+
+        #hay otro text-box que la invita a agregar otro item.
+        #ingresa: "Tirar la basura".
+
+        #hay otro text-box para usar ingresar otro item.
+        #ingresa: "Sacar la basura".
+
+        #la pagina se actualiza, y ahora muestra todos los items de la lista.
+
+        #Se genera un URL con la lista guardada y se explica con un texto.
+
+        #Al visitar ese URL su lista sigue estando ahi.
+
+        #usuario satisfecho.
+
+if __name__ == '__main__':
+    # unittest.main(warnings='ignore')
+    unittest.main()
