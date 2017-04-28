de2372803c3aaf85b8942ed601dd62b82a00795a
juan.rabino <juan.rabino@globant.com>
2017-04-28 16:35:31 -0300

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 from django.test import TestCase
 from django.http import HttpRequest
@@ -19,30 +19,26 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'Item nuevo en la lista'})
-
-        self.assertEqual(Item.objects.count(), 1)
-
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'Item nuevo en la lista' )
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'Item nuevo en la lista'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/la-unica-lista-del-mundo/')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'El primero de todos los items de la lista'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Segundo Item'
+        second_item.list = list_
         second_item.save()
+
+        seved_list = List.objects.first()
+        self.assertEqual(seved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -50,7 +46,21 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, first_item.text)
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, second_item.text)
+        self.assertEqual(second_item.list, list_)
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new', data={'item_text': 'Item nuevo en la lista'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'Item nuevo en la lista' )
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'Item nuevo en la lista'})
+        self.assertRedirects(response, '/lists/la-unica-lista-del-mundo/')
 
 class ListViewTest(TestCase):
 
@@ -60,8 +70,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response,'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemcillo 1')
-        Item.objects.create(text='itemcillo 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemcillo 1', list=list_)
+        Item.objects.create(text='itemcillo 2', list=list_)
 
         response = self.client.get('/lists/la-unica-lista-del-mundo/')
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,6 @@ from lists import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name = 'home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/la-unica-lista-del-mundo/$', views.view_list, name='view_list'),
 ]
