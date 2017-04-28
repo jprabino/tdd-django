881ca7abb8d401b40a8ed436081481449d77b988
Juan Pablo Rabino <juan.rabino@globant.com>
2017-04-28 10:25:29 -0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -4,9 +4,7 @@ import time
 import unittest
 
 
-
 class NewVisitorTest(unittest.TestCase):
-
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -22,35 +20,45 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        #Ingresar un item nuevo
-        inputbox  = self.browser.find_element_by_id('id_new_item')
+        # Ingresar un item nuevo
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Ingresar un nuevo item a la lista'
                          )
-        #Escribe "comprar tacho de basura" en un text box
+        # Escribe "comprar tacho de basura" en un text box
         inputbox.send_keys('comprar tacho de basura')
-        #cuando apreta 'Enter' se agraga el item con la leyenda
+        # cuando apreta 'Enter' se agraga el item con la leyenda
         # 1: Comprar tacho de basura
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table  = self.browser.find_element_by_id('id_list_table')
-        rows  = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: comprar tacho de basura' for row in rows))
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: comprar tacho de basura', [row.text for row in rows])
+        # hay otro text-box que la invita a agregar otro item.
+        # ingresa: "Tirar la basura".
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Tirar la basura')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: comprar tacho de basura', [row.text for row in rows])
+        self.assertIn('2: Tirar la basura', [row.text for row in rows])
         self.fail('Tests terminados!')
-        #hay otro text-box que la invita a agregar otro item.
-        #ingresa: "Tirar la basura".
 
-        #hay otro text-box para usar ingresar otro item.
-        #ingresa: "Sacar la basura".
+        # hay otro text-box para usar ingresar otro item.
+        # ingresa: "Sacar la basura".
 
-        #la pagina se actualiza, y ahora muestra todos los items de la lista.
+        # la pagina se actualiza, y ahora muestra todos los items de la lista.
 
-        #Se genera un URL con la lista guardada y se explica con un texto.
+        # Se genera un URL con la lista guardada y se explica con un texto.
 
-        #Al visitar ese URL su lista sigue estando ahi.
+        # Al visitar ese URL su lista sigue estando ahi.
 
-        #usuario satisfecho.
+        # usuario satisfecho.
 
 if __name__ == '__main__':
     # unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -9,8 +9,12 @@ from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
-
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
 
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data = {'item_text': 'Item nuevo en la lista'})
+        self.assertIn('Item nuevo en la lista', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
