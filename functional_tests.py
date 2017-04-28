3cdf21d3d0e815cc8c80dd5538bc33e97be73b87
juan.rabino <juan.rabino@globant.com>
2017-04-28 13:05:10 -0300

    Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -31,10 +36,8 @@ class NewVisitorTest(unittest.TestCase):
         # 1: Comprar tacho de basura
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: comprar tacho de basura')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: comprar tacho de basura', [row.text for row in rows])
         # hay otro text-box que la invita a agregar otro item.
         # ingresa: "Tirar la basura".
 
@@ -43,10 +46,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: comprar tacho de basura', [row.text for row in rows])
-        self.assertIn('2: Tirar la basura', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: comprar tacho de basura')
+        self.check_for_row_in_list_table('2: Tirar la basura')
         self.fail('Tests terminados!')
 
         # hay otro text-box para usar ingresar otro item.
