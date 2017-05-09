11e160a5ca283137fee8b582a365e61c4786ffe5
juan.rabino <juan.rabino@globant.com>
2017-05-09 14:18:36 -0300

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -16,35 +16,6 @@ class HomePageTest(TestCase):
 
         self.assertTemplateUsed(response, 'home.html')
 
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'El primero de todos los items de la lista'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Segundo Item'
-        second_item.list = list_
-        second_item.save()
-
-        seved_list = List.objects.first()
-        self.assertEqual(seved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, first_item.text)
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, second_item.text)
-        self.assertEqual(second_item.list, list_)
-
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
