1faa7226525ccec2c09c499696a45101e0a2d6f2
juan.rabino <juan.rabino@globant.com>
2017-05-10 15:21:18 -0300

    new form for list items

---
+++ lists/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from lists.models import Item
+EMPTY_ITEM_ERROR = 'No puede haber items vacíos'
+class ItemForm(forms.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {'text': forms.TextInput(attrs={
+                        'placeholder': 'Nueva Tarea a realizar',
+                        'class': 'form-control input-lg',}),
+                  }
+        error_messages = {
+                'text': {'required': EMPTY_ITEM_ERROR}
+        }
+    # item_text = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Nueva Tarea a realizar',
+    #                                                           'class': 'form-control input-lg',}))
