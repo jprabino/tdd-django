d79805a7033a37913c8f00b06b94372bc27a3e5d
juan.rabino <juan.rabino@globant.com>
2017-05-10 16:15:36 -0300

    Forms unit testing

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.utils.html import escape
 
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 from lists.models import Item, List
 from lists.views import home_page
 from django.test import TestCase
@@ -26,25 +26,38 @@ class HomePageTest(TestCase):
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/lists/new', data={'name': 'Item nuevo en la lista'})
+        response = self.client.post('/lists/new', data={'text': 'Item nuevo en la lista'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'Item nuevo en la lista' )
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'name': 'Item nuevo en la lista'})
+        response = self.client.post('/lists/new', data={'text': 'Item nuevo en la lista'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'name': ''})
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape('No se puede ingresar un item vacío')
-        self.assertContains(response, expected_error)
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    # def test_validation_errors_are_sent_back_to_home_page_template(self):
+    #     response = self.client.post('/lists/new', data={'name': ''})
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'home.html')
+    #     expected_error = escape('No se puede ingresar un item vacío')
+    #     self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
-        self.client.post('/lists/new', data={'name': ''})
+        self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
@@ -86,7 +99,7 @@ class ListViewTest(TestCase):
         other_list = List.objects.create()
         correct_list = List.objects.create()
         self.client.post(f'/lists/{correct_list.id}/',
-                         data={'name': 'Nuevo item de una lista existente'}
+                         data={'text': 'Nuevo item de una lista existente'}
                          )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -98,16 +111,16 @@ class ListViewTest(TestCase):
         other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.post(f'/lists/{correct_list.id}/',
-                                    data={'name': 'Nuevo item de una lista existente',})
+                                    data={'text': 'Nuevo item de una lista existente',})
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
     def test_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
         response = self.client.post(
             f'/lists/{list_.id}/',
-            data={'name': ''}
+            data={'text': ''}
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
-        expected_error = escape("No se puede ingresar un item vacío")
+        expected_error = escape(EMPTY_ITEM_ERROR)
         self.assertContains(response, expected_error)
