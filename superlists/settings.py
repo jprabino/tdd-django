5f054ca0aed973e8f7730252e313d57915700c44
juan.rabino <juan.rabino@globant.com>
2017-05-16 17:30:08 -0300

    deploying, updated settings to do some logging

--- superlists/settings.py
+++ superlists/settings.py
@@ -134,3 +134,20 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'jprabino@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
