da9767ccaa34c1f29a91681803e396acb82a5cc5
juan.rabino <juan.rabino@globant.com>
2017-05-22 15:38:28 -0300

    git commit -am "add screenshot on failure to FT runner"

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,12 +4,16 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 import os
+from datetime import datetime
 from .server_tools import reset_database
 MAX_WAIT = 10
 STAGING_SERVER="10.210.8.206"
 # #STAGING_SERVER='localhost:8000'
 # os.environ['STAGING_SERVER']=STAGING_SERVER
 
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 def wait(fn):
 
@@ -34,9 +38,44 @@ class FunctionalTest(StaticLiveServerTestCase):
         if self.staging_server:
             setattr(self, 'live_server_url', 'http://' + self.staging_server)
             reset_database(self.staging_server)
-    def tearDown(self):
-        self.browser.quit()
 
+    def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to.window(handle)
+                self.take_screenshot()
+                self.dump_html()
+
+        self.browser.quit()
+        super().tearDown()
+
+    def _test_has_failed(self):
+
+        return any(error for (method, error) in self._outcome.errors)
+
+    def take_screenshot(self):
+        filename = self._get_filename() + ".png"
+        print('screenshooting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder = SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
     def add_list_item(self, item_text):
         num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
         self.get_item_input_box().send_keys(item_text)
